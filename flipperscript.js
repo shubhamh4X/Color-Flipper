546cae8185eea9c7150ed36aadafdde630eeea56
Shubham Das <121857404+shubhamh4X@users.noreply.github.com>
2024-12-13 16:51:21 +0530

    Add files via upload

---
+++ flipperscript.js
@@ -0,0 +1,19 @@
+function getRandomColor() 
+{
+    
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+}
+
+
+document.getElementById('flipper').addEventListener('click', changeBackgroundColor);
